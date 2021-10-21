9931e7fb4c0b8e6aa722d0fe7cd859e98cd3a525
임또이 <sh663746@gmail.com>
2021-10-21 20:01:10 +0900

    init

---
+++ src/test/java/com/example/Rsupport/RsupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
